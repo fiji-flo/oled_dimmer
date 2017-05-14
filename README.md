51399a2bdf89d854d9a9571ffebac3c42cbf76d8
Florian Merz <flomerz@gmail.com>
2017-05-14 21:58:26 +0200

    updated version based on system brightness

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# oled_dimmer
-GNOME extension for OLED display dimming
+# OLED DIMMER: GNOME extension for OLED display dimming
+
+Unfortunately, regular display dimming does not work on OLED displays. Regular
+dimming is done by reducing the brightness of the display backlight. However,
+OLED displays don't have a backlight. Therefore, dimming has to be done by
+reducing the brightness of the scene.
+
+## A simple GNOME extension to the rescue
+
+This is a simple GNOME extension which:
+
+* Attaches to the brightness level of the GNOME settings daemon.
+* Adds an effect [ClutterBrightnessContrastEffect](https://developer.gnome.org/clutter/stable/ClutterBrightnessContrastEffect.html).
+* Keeps them in sync.
+
+## Installation
+
+```
+git clone https://github.com/fiji-flo/oled_dimmer.git ~/.local/share/gnome-shell/extensions/oled_dimmer@flomerz.gmail.com
+gnome-shell-extension-tool -e oled_dimmer@flomerz.gmail.com
+```
+
+## Known issues
+
+* Cursor is not affected by dimming.
--- extension.js
+++ extension.js
@@ -1,92 +1,60 @@
 /* global global imports */
-const St = imports.gi.St;
-const Meta = imports.gi.Meta;
 const Lang = imports.lang;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 const Clutter = imports.gi.Clutter;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Slider = imports.ui.slider;
+const Gio = imports.gi.Gio;
 
-let text, button, onWindowCreated, slider, darker;
-let level = -0.2;
+const BUS_NAME = 'org.gnome.SettingsDaemon.Power';
+const OBJECT_PATH = '/org/gnome/SettingsDaemon/Power';
+
+const BrightnessInterface = '<node> \
+<interface name="org.gnome.SettingsDaemon.Power.Screen"> \
+<property name="Brightness" type="i" access="readwrite"/> \
+</interface> \
+</node>';
+
+const BrightnessProxy = Gio.DBusProxy.makeProxyWrapper(BrightnessInterface);
+
+let text, dimmer, onWindowCreated, slider, darker;
 
 const Darker = new Lang.Class({
   Name: 'Darker',
   _init: function(actor) {
     this._effect = new Clutter.BrightnessContrastEffect();
-    this._effect.set_brightness(level);
     actor.add_effect(this._effect);
     this.actor = actor;
   },
 })
 
 function slide() {
-  level = slider.value - 1.0;
-  darker._effect.set_brightness(level);
 }
 
-const DimmerMenu = Lang.Class({
-  Name: 'DimmerMenu',
-  Extends: PanelMenu.Button,
+const Dimmer = new Lang.Class({
+  Name: 'Dimmer',
   _init: function() {
-    this.parent(0.0, _('OLED Dimmer'));
-    let buttonIcon = new St.Icon({
-      icon_name: 'display-brightness-symbolic',
-      style_class: 'system-status-icon'
-    });
-    this.actor.add_actor(buttonIcon);
-    let sliderItem = new PopupMenu.PopupMenuItem('', {
-      style_class: 'popup-menu-icon'
-    });
-    let sliderValue = level + 1.0;
-    let sliderIcon = new St.Icon({
-      icon_name: 'display-brightness-symbolic',
-      style_class: 'popup-menu-icon'
-    });
-    sliderItem.actor.add(sliderIcon);
-    slider = new Slider.Slider(sliderValue);
-    slider.connect('value-changed', slide);
-    sliderItem.actor.add(slider.actor, { expand: true });
-    sliderItem.actor.connect('key-press-event', Lang.bind(sliderItem, function(actor, event) {
-      return slider.onKeyPressEvent(actor, event);
-    }));
-    this.menu.addMenuItem(sliderItem);
+    this._proxy = new BrightnessProxy(
+      Gio.DBus.session, BUS_NAME, OBJECT_PATH,
+      Lang.bind(this, function(proxy, error) {
+        if (error) {
+          global.log(error.message);
+          return;
+        }
+        this._proxy.connect('g-properties-changed', Lang.bind(this, this._sync));
+        this._sync();
+      })
+    );
+  },
+  _sync: function() {
+    let level = this._proxy.Brightness / 100.0 - 1.0;
+    darker._effect.set_brightness(level);
   }
 });
-
-function _hideLevel() {
-  Main.uiGroup.remove_actor(text);
-  text = null;
-}
-
-function _showLevel() {
-  if (!text) {
-    text = new St.Label({ style_class: 'darker-label', text: `${level}` });
-    Main.uiGroup.add_actor(text);
-  }
-
-  text.opacity = 255;
-
-  let monitor = Main.layoutManager.primaryMonitor;
-
-  text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                    monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-  Tweener.addTween(text,
-                   { opacity: 0,
-                     time: 2,
-                     transition: 'easeOutQuad',
-                     onComplete: _hideLevel });
-}
 
 function init() {
 }
 
 function enable() {
-  button = new DimmerMenu();
-  Main.panel.addToStatusArea('dimmer', button, 0);
+  dimmer = new Dimmer();
   let actor = Main.uiGroup;
   darker = new Darker(actor);
 }
@@ -97,5 +65,5 @@ function disable() {
     actor.remove_effect(darker._effect);
     delete darker;
   }
-  button.destroy();
+  delete dimmer;
 }
